4a59d0d7695fbee937e09b0323652124ed0c4bf8
Gavin.Kimpson <gkimpson@gmail.com>
2017-07-29 13:54:43 +0100

    S7L32: added controllers to api routes with only param, use php artisan route:list to view all potential routes - forgot the ProductController (also removed the routes from web.php)

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('transactions', 'Transaction\TransactionController', ['except' => ['create', 'edit']]);
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
